32673dfe4132c113722575110414c746deb94882
Elizabeth O. Segun-Ajiboye <elizabethaderemi@gmail.com>
2021-06-03 21:36:01 +0100

    handling responsiveness in the navbar

---
+++ public/index.js
@@ -0,0 +1,10 @@
+const burger = document.querySelector("#navBurger");
+const menu = document.querySelector("#navMenu");
+
+burger.addEventListener("click", () => {
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden");
+  }
+});
